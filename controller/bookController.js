c8df986dc5f776e09c5badef9b6c14446869c304
Aman <amanmohadikar111@gmail.com>
2022-11-28 16:45:59 +0530

    all ok

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,39 +2,50 @@ const { mongo, default: mongoose } = require("mongoose");
 const bookModel = require("../model/bookModel")
 const userModel = require("../model/userModel")
 
+const nameRegex = /^[a-zA-Z ]+$/
 
-/*
-- Create a book document from request body. Get userId in request body only.
-- Make sure the userId is a valid userId by checking the user exist in the users collection.
-- Return HTTP status 201 on a succesful book creation. Also return the book document. The response should be a JSON object like [this](#successful-response-structure) 
-- Create atleast 10 books for each user
-- Return HTTP status 400 for an invalid request with a response body like [this](#error-response-structure)
-*/
+
+
 const createBook = async function (req, res) {
-    let data = req.body
-    let { title, excerpt, userId, ISBN, category, subcategory, releasedAt } = data;
-    if (!title) return res.status(400).send({ status: false, message: "Title is not present" })
+    try {
+        let data = req.body
+        let { title, excerpt, userId, ISBN, category, subcategory, releasedAt } = data;
 
-    if (!excerpt) return res.status(400).send({ status: false, message: "excerpt is not present" })
+        if (!title) return res.status(400).send({ status: false, message: "Title is not present" })
+        if (!title.match(nameRegex)) return res.status(400).send({ status: false, message: "title is not valid" })
+        let isTitlePresent = await bookModel.findOne({ title: title })
+        if (isTitlePresent) return res.status(400).send({ status: false, message: "Title is already exist" })
 
-    if (!userId) return res.status(400).send({ status: false, message: "userId is not present" })
-    const isUserIdPresent = await userModel.findOne({ userId: userId })
-    if (!isUserIdPresent) return res.status(400).send({ status: false, message: "user Id not exist" })
-    if (!mongoose.Types.ObjectId.isValid(userId)) return res.status(400).send({ status: false, message: "userId not valid" })
+        if (!excerpt) return res.status(400).send({ status: false, message: "excerpt is not present" })
+        if (!excerpt.match(nameRegex)) return res.status(400).send({ status: false, message: "excerpt is not valid" })
 
-    if(!ISBN) return res.status(400).send({status : false, message : "ISBN is not present"})
+        if (!userId) return res.status(400).send({ status: false, message: "userId is not present" })
+        const isUserIdPresent = await userModel.findById(userId)
+        if (!mongoose.Types.ObjectId.isValid(userId)) return res.status(400).send({ status: false, message: "userId not valid" })
+        if (!isUserIdPresent) return res.status(400).send({ status: false, message: "user Id not exist" })
 
-    if(!category) return res.status(400).send({status : false, message : "category is not present"})
+        if (!ISBN) return res.status(400).send({ status: false, message: "ISBN is not present" })
+        if (Object.keys(ISBN).length != 10 && Object.keys(ISBN).length != 13) return res.status(400).send({ status: false, message: "ISBN is not valid" })
+        let isISBNPresent = await bookModel.findOne({ ISBN: ISBN })
+        if (isISBNPresent) return res.status(400).send({ status: false, message: "ISBN is already exist" })
 
-    if(!subcategory) return res.status(400).send({status : false, message : "subcategory is not present"})
+        if (!category) return res.status(400).send({ status: false, message: "category is not present" })
+        if (!category.match(nameRegex)) return res.status(400).send({ status: false, message: "category is not valid" })
 
-    if(!releasedAt) return res.status(400).send({status : false, message : "releasedAt is not present"})
+        if (!subcategory) return res.status(400).send({ status: false, message: "subcategory is not present" })
+        if (!subcategory.match(nameRegex)) return res.status(400).send({ status: false, message: "subcategory is not valid" })
 
-    let newData = await bookModel.create(data)
+        if (!releasedAt) return res.status(400).send({ status: false, message: "releasedAt is not present" })
 
-    return res.status(400).send({status : true, message : "Book Successfully create", data : newData})
+        let newData = await bookModel.create(data)
 
-
+        return res.status(400).send({ status: true, message: "Book Successfully create", data: newData })
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
 }
+
+
 
 module.exports.createBook = createBook
