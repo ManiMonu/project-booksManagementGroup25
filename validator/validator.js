cc93eb15f220b9c84718c8019ddbc48e2c27ee1c
KLChakraborty <chakrabortykajal2@gmail.com>
2022-12-26 13:49:28 +0530

    Done

--- validator/validator.js
+++ validator/validator.js
@@ -4,7 +4,7 @@ const requiredInput = function(requestBody){
 
 const validInput = function(value){
    if(typeof value === 'undefined' || value === null) return false
-    if(typeof value === 'string' && value.trim().length === 0) return false
+    if(typeof value === 'string' && value.trim().length === 0) return false 
     if(typeof value !== 'string') return false
     return true
 }
